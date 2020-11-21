72021130be4cc05623cbff17ed0d2e05fbf0dc3e
hanjuren <juren528@gmail.com>
2020-11-22 03:12:41 +0900

    헤더 푸터 분리

---
+++ js/include.js
@@ -0,0 +1,15 @@
+includeHTML(document.querySelector('#footer'), '/item/footer.html');
+includeHTML(document.querySelector('#header'), '/item/header.html');
+
+
+function includeHTML(divContainer, urlHTML) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+        if (this.status == 200) { divContainer.innerHTML = this.responseText; }
+        if (this.status == 404) { divContainer.innerHTML = "올바르지 않은 페이지 경로입니다."; }
+    }
+}
+xhttp.open("GET", urlHTML, true);
+xhttp.send();
+}
